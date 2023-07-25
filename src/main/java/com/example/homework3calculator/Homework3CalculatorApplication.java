24f460661a728ccda493ba0b82ca159ef0be864e
Admin <samedi450511@gmail.com>
2023-07-25 15:29:18 +0600

    start

---
+++ src/main/java/com/example/homework3calculator/Homework3CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework3calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework3CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework3CalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework3calculator/Homework3CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework3calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework3CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
